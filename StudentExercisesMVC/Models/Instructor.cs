08b1f09eb84ecc57443c88d9d8acc5fa4ba8834f
Emily Loggins <em.loggins7@gmail.com>
2019-08-13 09:43:57 -0500

    new index

--- StudentExercisesMVC/Models/Instructor.cs
+++ StudentExercisesMVC/Models/Instructor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,12 +8,23 @@ namespace StudentExercisesMVC.Models
 {
     public class Instructor
     {
+        [Required]
         public int Id { get; set; }
+        [Required]
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
+        [Required]
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
+        [Required]
+        [Display(Name = "Slack Handle")]
         public string SlackHandle { get; set; }
+        [Required]
         public string Specialty { get; set; }
+        [Required]
+        [Display(Name = "Cohort Id")]
         public int CohortId { get; set; }
+        [Display(Name = "Full Name")]
         public string FullName
         {
             get
